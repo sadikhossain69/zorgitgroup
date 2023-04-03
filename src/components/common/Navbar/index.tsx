3cc536fb29c33eb1b277d9844977158b98659ccb
sadikhossain69 <sadikhossain433@gmail.com>
2023-04-03 08:34:30 +0600

    logo part complete

--- src/components/common/Navbar/index.tsx
+++ src/components/common/Navbar/index.tsx
@@ -5,10 +5,13 @@ import { Link } from 'react-router-dom';
 import Logo from '@images/brand-logo.png';
 import IconGroup from '../IconGroup';
 import NavLink, { links } from './NavLink';
+import smallLogo from "@images/smallLogo.jpeg"
+import { useWindowSize } from '@react-hook/window-size'
 
 export default function Navbar() {
   const [navbarOpen, setNavbarOpen] = useState(false);
   const [isScrolling, setIsScrolling] = useState(false);
+  const [width, height] = useWindowSize();
 
   const ChangeBackground = () => {
     if (window.scrollY >= 80) {
@@ -25,26 +28,23 @@ export default function Navbar() {
   return (
     <>
       <nav
-        className={`fixed bg-white md:bg-transparent ${
-          isScrolling && 'md:bg-white shadow'
-        } top-0 z-50 flex flex-wrap items-center justify-between w-full mb-3 md:pr-2  `}
+        className={`fixed bg-white md:bg-transparent ${isScrolling && 'md:bg-white shadow'
+          } top-0 z-50 flex flex-wrap items-center justify-between w-full mb-3 md:pr-2  `}
       >
         <div
-          className={`md:container flex flex-wrap items-center justify-between mx-auto ${
-            navbarOpen && 'h-screen md:h-auto'
-          } ${!navbarOpen && 'w-full'}`}
+          className={`md:container flex flex-wrap items-center justify-between mx-auto ${navbarOpen && 'h-screen md:h-auto'
+            } ${!navbarOpen && 'w-full'}`}
         >
           <div className="relative z-50 flex justify-between w-full px-2 md:px-0 md:w-auto md:static">
             <Link
-              className="inline-block py-2 mr-4 text-sm font-bold leading-relaxed uppercase whitespace-nowrap "
+              className={`inline-block py-2 mr-4 text-sm font-bold leading-relaxed uppercase whitespace-nowrap `}
               to="/"
             >
-              <img src={Logo} alt="company logo" />
+              <img className={`${width < 768 && "w-28"}`} src={`${width < 768 ? smallLogo : Logo}`} alt="company logo" />
             </Link>
             <button
-              className={`${
-                navbarOpen ? `rotate-180` : `-rotate-180`
-              }  transition-transform ease-in-out duration-700 block px-3 py-1 text-xl leading-none bg-transparent border border-transparent border-solid rounded outline-none cursor-pointer md:hidden focus:outline-none`}
+              className={`${navbarOpen ? `rotate-180` : `-rotate-180`
+                }  transition-transform ease-in-out duration-700 block px-3 py-1 text-xl leading-none bg-transparent border border-transparent border-solid rounded outline-none cursor-pointer md:hidden focus:outline-none`}
               type="button"
               onClick={() => setNavbarOpen(!navbarOpen)}
             >
@@ -52,17 +52,15 @@ export default function Navbar() {
             </button>
           </div>
           <div
-            className={`md:flex flex-col w-screen md:w-auto md:mr-auto ${
-              navbarOpen ? 'h-full' : 'h-0 md:-mt-12'
-            }`}
+            className={`md:flex flex-col w-screen md:w-auto md:mr-auto ${navbarOpen ? 'h-full' : 'h-0 md:-mt-12'
+              }`}
             id="example-navbar-danger"
           >
             <ul
-              className={`flex w-full md:mt-0 md:bg-transparent transition-all bg-white md:transition-none ease-in-out duration-700 flex-col pt-5 list-none md:pt-0 md:flex-row lg:ml-auto ${
-                navbarOpen
+              className={`flex w-full md:mt-0 md:bg-transparent transition-all bg-white md:transition-none ease-in-out duration-700 flex-col pt-5 list-none md:pt-0 md:flex-row lg:ml-auto ${navbarOpen
                   ? 'translate-y-0 opacity-100'
                   : ' -translate-y-full opacity-0 md:opacity-100 md:translate-y-0 w-full'
-              }`}
+                }`}
             >
               {links.map((link) => (
                 <NavLink key={link.title} title={link.title} to={link.to} />
