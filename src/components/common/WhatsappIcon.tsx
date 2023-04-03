92a2317555f5476fe55b802f963378f4552b8218
sadikhossain69 <sadikhossain433@gmail.com>
2023-04-03 09:17:45 +0600

    whatsapp icon rewite

--- src/components/common/WhatsappIcon.tsx
+++ src/components/common/WhatsappIcon.tsx
@@ -3,10 +3,17 @@ import { AiOutlineWhatsApp } from 'react-icons/ai';
 
 const WhatsappIcon = () => {
     return (
-        <button className='fixed bottom-16 right-5 md:bottom-24 md:right-8 z-50'>
-            <a href="https://wa.me/message/VUQHDAMVQEK5A1" target={'_blank'}>
-                <AiOutlineWhatsApp className={`bg-gradient-to-tr from-[#48f15d] to-[#3f4839c2] text-white p-2 rounded-full hover:bg-white hover:duration-300 duration-300 h-8 w-8 md:h-12 md:w-12`} />
-            </a>
+        <button className='fixed bottom-16 right-4 md:bottom-24 md:right-4 z-50'>
+            <div className='flex flex-col'>
+                <a href="https://wa.me/message/VUQHDAMVQEK5A1" target={'_blank'} className='mx-auto'>
+                    <AiOutlineWhatsApp className={`bg-green-500  text-white p-2 rounded-full hover:bg-white hover:text-green-700 hover:duration-300 duration-300 h-8 w-8 md:h-12 md:w-12 shadow-lg`} />
+                </a>
+                <a href="https://wa.me/message/VUQHDAMVQEK5A1" target={'_blank'}>
+                    <p className='w-12 md:w-auto shadow-lg py-1 px-2 bg-white rounded-lg mt-1'>
+                        Live Chat
+                    </p>
+                </a>
+            </div>
         </button>
     );
 };
